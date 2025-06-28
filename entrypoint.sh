edb44e0eaeed0968c3e95f0729ad5dd508c0cb17
Kamadee <tahieuthang@gmail.com>
2025-06-28 21:06:32 +0700

    update docker-compose

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$APP_ROLE" = "worker" ]; then
-  echo "Running Laravel queue worker..."
-  php artisan queue:work
-else
-  echo "Running Laravel web server..."
-  php artisan serve --host=0.0.0.0 --port=8000
-fi
