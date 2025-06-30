48912cbf6996c4e6d97e910401ecfc8c1e21e328
Kamadee <tahieuthang@gmail.com>
2025-06-30 11:23:24 +0700

    update seeder 2

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,23 +1,22 @@
 <?php
-
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('users')->insert([
-            ['username' => 'ADMIN', 'password' => Hash::make('Admin@123'), 'role' => '1'],
-            ['username' => 'test', 'password' => Hash::make('Admin@123'), 'role' => '2'],
-            ['username' => 'hieuth', 'password' => Hash::make('Admin@123'), 'role' => '2'],
-            ['username' => 'sonNN', 'password' => Hash::make('Admin@123'), 'role' => '2'],
-        ]);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    DB::table('users')->insert([
+      ['username' => 'ADMIN', 'password' => Hash::make('Admin@123'), 'role' => '1'],
+      ['username' => 'test', 'password' => Hash::make('Admin@123'), 'role' => '2'],
+      ['username' => 'hieuth', 'password' => Hash::make('Admin@123'), 'role' => '2'],
+      ['username' => 'sonNN', 'password' => Hash::make('Admin@123'), 'role' => '2'],
+    ]);
+  }
 }
