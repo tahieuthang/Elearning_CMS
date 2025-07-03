f5c6b9753f04ce0a94a9495b6b6940e95f6cbde4
Kamadee <tahieuthang@gmail.com>
2025-07-03 21:59:34 +0700

    update video d=list view

--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -78,7 +78,8 @@
 @section('js')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+<!-- ✅ Bootstrap sau -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script>
