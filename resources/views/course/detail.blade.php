369287b052f41215fa54c24bc5a36218d0ec4bfc
Kamadee <tahieuthang@gmail.com>
2025-07-03 15:48:44 +0700

    update course detail view

--- resources/views/course/detail.blade.php
+++ resources/views/course/detail.blade.php
@@ -18,6 +18,7 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet">
 
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap4-theme/1.0.0/select2-bootstrap4.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
 <!-- FindPond -->
 <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet" />
 <!-- FileInput -->
@@ -37,6 +38,7 @@
 <script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
 <script src="https://unpkg.com/filepond/dist/filepond.js"></script>
     <!-- FileInput -->
