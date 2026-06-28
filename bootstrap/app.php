5717abf5d7dcc133c21b925cf5bd27d021371042
Kamadee <tahieuthang@gmail.com>
2026-06-28 21:36:17 +0700

    feat(sentry): integrate sentry sdk for backend exception tracking

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,5 +19,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->redirectGuestsTo('/auth/login');
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        \Sentry\Laravel\Integration::handles($exceptions);
     })->create();
